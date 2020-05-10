d2fc038db49908abe3274d4c4fbe50cd063af293
KaranJain29 <51224137+KaranJain29@users.noreply.github.com>
2020-05-10 14:51:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,17 +2,11 @@
 /**
  * ====================================================================================
  *                           SUBTITLE MANAGER (c) KARAN KANKARIA
- * ----------------------------------------------------------------------------------
- * @copyright This software is exclusively sold at CodeCanyon.net. If you have downloaded this
- *  from another site or received it from someone else than me, then you are engaged
- *  in an illegal activity. You must delete this software immediately or buy a proper
- *  license from http://codecanyon.net/user/karankankaria/
- *
- *  Thank you for your cooperation and don't hesitate to contact me if anything :)
  * ====================================================================================
  *
- * @author KARAN KANKARIA (https://linkshrink.online/subtitle-manager/)
- * @link https://linkshrink.online/subtitle-manager/
+ *  Thank you for your cooperation and don't hesitate to contact me if anything :)
+ * @author KARAN KANKARIA (https://subtitle-manager.herokuapp.com/index.php)
+ * @link https://subtitle-manager.herokuapp.com/index.php
  * @package SUBTITLE MANAGER
  */
 session_start();
