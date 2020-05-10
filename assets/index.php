2551fa4404caf87c21b8d79a72e00f3aaa80c627
KaranJain29 <51224137+KaranJain29@users.noreply.github.com>
2020-05-10 14:02:01 +0530

    Create index.php

---
+++ assets/index.php
@@ -0,0 +1,11 @@
+<?php 
+/**
+ * ====================================================================================
+ *                           SUBTITLE MANAGER (c) KARAN KANKARIA
+ * ====================================================================================
+ *
+ * @author KARAN KANKARIA (https://linkshrink.online/subtitle-manager/)
+ * @link https://linkshrink.online/subtitle-manager/
+ * @package SUBTITLE MANAGER
+ */
+ ?>
