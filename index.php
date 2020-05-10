9940aa0ec7bab0355302a6d6527e3a23a89d5c27
KaranJain29 <51224137+KaranJain29@users.noreply.github.com>
2020-05-10 14:12:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -120,7 +120,7 @@ function logged_in_msg($username)
  * @package SUBTITLE MANAGER
  */
 			$domainServer = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"; 
-			$domainServer = str_replace('', '', $domainServer);
+			$domainServer = str_replace('index.php', '', $domainServer);
 		?>
 <!DOCTYPE html>
 <html lang="en">
